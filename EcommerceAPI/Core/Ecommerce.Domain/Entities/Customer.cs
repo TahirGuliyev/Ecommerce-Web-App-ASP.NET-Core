cf2c6a5daddbc1972f803170236335eb674e3d15
Tahir Guliyev <tahirguliyev95@gmail.com>
2023-01-11 12:12:32 +0400

    DesignTimeFactory, Configuration are added

---
+++ EcommerceAPI/Core/Ecommerce.Domain/Entities/Customer.cs
@@ -0,0 +1,10 @@
+﻿using Ecommerce.Domain.Entities.Common;
+
+namespace Ecommerce.Domain.Entities
+{
+    public class Customer : BaseEntity
+    {
+        public string Name { get; set; }
+        public ICollection<Order> Orders { get; set; }
+    }
+}
